a223c4b4ab27698fc75e91968313a66dfa5bf2d1
Benjamin Maudslay <benjaminmaudslay@Benjamins-MacBook-Pro-1129.local>
2021-09-23 11:20:58 +0100

    final touches

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,52 @@
-import { useState } from "react"
+import { useState } from "react";
 import { evaluate } from "mathjs";
+
+import "./App.css";
 import keys from "./keys.json";
+import { Key } from "./components/Key";
+import { Display } from "./components/Display";
 
 const App = () => {
-  const [total, setTotal] = useState([0])
+  const [total, setTotal] = useState([0]);
 
   const handleClick = (val) => {
     if (val === "clear") {
-      setTotal([0])
+      setTotal([0]);
     } else if (val === "=") {
-      let currentStr = total.join("")
-      let newTotal = evaluate(currentStr)
-      setTotal(newTotal)
+      let currentStr = total.join("");
+      let newTotal = evaluate(currentStr);
+      // limit number of chars for new total
+      setTotal(newTotal);
     } else {
-      let newTotal = [...total, val]
-      if(total[0] === 0) {
-        newTotal.shift()
+      let newTotal = [...total, val];
+      if (total[0] === 0) {
+        newTotal.shift();
       }
-      setTotal(newTotal)
+      setTotal(newTotal);
     }
   };
 
   return (
-    <div>
+    <div className="wrapper">
       <h1>React Calculator</h1>
-      <Display val={total} />
-      {keys.map((item, index) => {
-        return (
-          <Key
-            key={index}
-            val={item.val}
-            alias={item.alias}
-            handleClick={handleClick}
-          />
-        );
-      })}
+      <div className="calcContainer">
+        <Display val={total} />
+        <div className="keyContainer">
+          {keys.map((item, index) => {
+            return (
+              <Key
+                key={index}
+                val={item.val}
+                alias={item.alias}
+                style={item.style}
+                handleClick={handleClick}
+              />
+            );
+          })}
+        </div>
+      </div>
     </div>
   );
-};
-
-const Display = (props) => {
-  return <h3>{props.val}</h3>;
-};
-
-const Key = ({ alias, val, handleClick }) => {
-  return <button onClick={() => handleClick(val)}>{alias ? alias : val}</button>;
 };
 
 export default App;
